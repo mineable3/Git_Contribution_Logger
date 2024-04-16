1f7b49a8cc2521a9bd9ca092d17463d80555d3ac
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-04-16 10:12:58 -0400

    logging script

---
+++ logger.ps1
@@ -0,0 +1,9 @@
+$fileContent = Get-Content .\log.txt
+$nextCommit = $fileContent + '1'
+$date = Get-Date
+
+Out-File -FilePath .\log.txt -Encoding ascii -InputObject $nextCommit
+
+git add .\log.txt
+git commit -m $date
+git push origin main
